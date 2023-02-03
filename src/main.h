963bebcfbde44b3ac6620fb077872c30f0247dc0
Brett <brett@dbzoo.com>
2023-02-03 19:16:41 +0000

    Extend idle timeout, add ombp proxy sample

--- src/main.h
+++ src/main.h
@@ -26,7 +26,7 @@
 
 /* Global variables for the main controls of the program */
 #define MAXBUFFSIZE     ((size_t)(1024 * 96))   /* Max size of buffer */
-#define MAX_IDLE_TIME   (60 * 10)       /* 10 minutes of no activity */
+#define MAX_IDLE_TIME   (60 * 30)       /* 30 minutes of no activity */
 
 /* Global Structures used in the program */
 extern struct config_s *config;
